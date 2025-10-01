ff375d62703036eaf816ed297a675991431323d2
Jhon Jairo Sanchez Benavides <jhonjairo@escala.com>
2025-10-01 14:00:40 -0500

    Register integration models in Django admin

--- integrations/admin.py
+++ integrations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Integration, IntegrationAction, IntegrationTrigger
+
+admin.site.register(Integration)
+admin.site.register(IntegrationAction)
+admin.site.register(IntegrationTrigger)
