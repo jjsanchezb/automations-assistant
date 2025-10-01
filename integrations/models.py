5de81fca701b7cf1885639e36a9ffbe012fa1c1f
Jhon Jairo Sanchez Benavides <jhonjairo@escala.com>
2025-10-01 12:52:38 -0500

    Add source code and runtime choice enums

--- integrations/models.py
+++ integrations/models.py
@@ -1,15 +1,7 @@
 import uuid_utils as uuid
 from django.db import models
 
-
-class SourceCodeLocation(models.TextChoices):
-    IN_APP_MODULE = "in_app_module", "In-App Module (Bundled)"
-    S3_ARTIFACT = "s3_artifact", "S3 Bucket Artifact (Dynamic)"
-
-
-class RuntimeChoices(models.TextChoices):
-    SUBPROCESS = "subprocess", "Subprocess"
-    CONTAINER = "container", "Container"
+from .choices import RuntimeChoices, SourceCodeLocation
 
 
 class Integration(models.Model):
